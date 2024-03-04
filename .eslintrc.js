e166c178c23ec498d3a7a7136f0bfdb0afafdef2
Shehan Wijethunga <pwshehan@gmail.com>
2024-03-04 23:57:16 +0530

    refactor: lint and husky config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,32 @@
 module.exports = {
   root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'jest'],
+  parser: '@typescript-eslint/parser', // Use TypeScript parser
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json', // Path to your TypeScript config
+  },
+  env: {
+    es6: true,
+    node: true,
+  },
+  plugins: [
+    '@typescript-eslint', // Add the TypeScript plugin
+  ],
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:jest/recommended',
-    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended', // TypeScript recommended rules
+    'plugin:prettier/recommended'
   ],
-  env: {
-    node: true,
-    jest: true,
+  rules: {
+    // TypeScript-specific rules:
+    '@typescript-eslint/no-unused-vars': 'warn',
+    // ... other rules (both general JS and TypeScript)
   },
-  rules: {},
+  overrides: [
+    {
+      files: ['src/**/*.{js,jsx,ts,tsx}'],
+      excludedFiles: '**/node_modules/**',
+    }
+  ]
 };
