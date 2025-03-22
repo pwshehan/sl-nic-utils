1dd3560300cb84501d074977f5842d4bd5bbe568
Shehan Wijethunga <pwshehan@gmail.com>
2025-03-22 17:33:42 +0530

    chore: code format

--- tests/format-nic.test.ts
+++ tests/format-nic.test.ts
@@ -3,11 +3,11 @@ import NIC from '../src/';
 describe('formatNic', () => {
   test('should return new NIC for valid old NIC', () => {
     const nic = new NIC('132417867V');
-    expect(nic.format('NEW')).toBe("191324107867");
+    expect(nic.format('NEW')).toBe('191324107867');
   });
 
   test('should return old NIC for valid new NIC', () => {
     const nic = new NIC('191324107867');
-    expect(nic.format('OLD')).toBe("132417867V");
+    expect(nic.format('OLD')).toBe('132417867V');
   });
 });
